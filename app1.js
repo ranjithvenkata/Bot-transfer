4ff86029760d3b744b9957035b1cb29188840bcd
Ranjith Venkata <st118174@ait.asia>
2018-03-23 11:25:14 +0530

    working

---
+++ app1.js
@@ -0,0 +1,66 @@
+var io = require('socket.io-client');
+var ss = require('socket.io-stream');
+var socket = io.connect('http://localhost:3000');
+// var stream = ss.createStream();
+var fs = require('fs');
+const path = require('path');
+var chokidar = require('chokidar');
+const clientpath = '/home/ranjith/Documents/Client2/';
+var watcher = chokidar.watch(clientpath, {ignored: /^\./, persistent: true, ignoreInitial: true});
+var unzip = require('unzip');
+var count = 0;
+
+//unzip Master content
+
+if(count == 0 ) //initial add- chokidar  
+		{ss(socket).on('master-sent', function(stream) {
+		console.log("recieving Master folder");
+		console.log('unzipping master back');
+		stream.pipe(unzip.Extract({ path: clientpath }));
+		console.log("recieved Master Folder");}); }
+
+watcher
+  .on('add', function(path) {
+		console.log("Client watcher called");
+		console.log('File', path, 'has been added, being to sent to server');
+		var stream = ss.createStream();
+		ss(socket).emit('client-added', stream, {name: path});
+		fs.createReadStream(path).pipe(stream);
+		count++;
+	})
+   .on('change', function(path) {console.log('File', path, 'has been changed, being to sent to server');
+				var stream = ss.createStream();
+				ss(socket).emit('client-modified', stream, {name: path});
+				fs.createReadStream(path).pipe(stream);
+				})
+  .on('unlink', function(path) {console.log('File', path, 'has been removed, Filename being sent to server');
+				var stream = ss.createStream();
+				ss(socket).emit('client-removed',{name: path});
+				});
+
+
+
+
+
+  // .on('error', function(error) {console.error('Error happened', error);})
+	// ss(socket).emit('profile-image1', stream);
+	// stream.pipe(fs.createWriteStream('/home/ranjith/Documents/Client1/' + filename));
+	ss(socket).on('server-added', function(stream, data) {
+		console.log("recieving new file");
+		var filename = path.basename(data.name);
+	stream.pipe(fs.createWriteStream(clientpath + filename)); });
+	ss(socket).on('server-modified', function(stream, data) {
+		console.log("recieving modified file");
+		var filename = path.basename(data.name);
+	stream.pipe(fs.createWriteStream(clientpath + filename, {flags: 'w'}));});
+	ss(socket).on('server-removed', function(data) {
+		console.log("recieving removed file name");
+		var filename = path.basename(data.name);
+	fs.unlink(clientpath + filename, function(error) {
+   			 if (error) {
+        			console.log("Error:File not deleted");
+				
+    				}else{console.log("File deleted");}});
+
+
+});
---
+++ server.js
@@ -0,0 +1,142 @@
+"use strict";
+var HashMap = require('hashmap');
+const express = require('express');
+const app = express();
+const http = require('http');
+const httpServer = http.Server(app);
+const io = require('socket.io')(httpServer);
+const SocketIOFile = require('socket.io-file');
+const path = require('path');
+var ss = require('socket.io-stream');
+const fs = require('fs');
+var hmap = new HashMap();;
+var chokidar = require('chokidar');
+const datapath = '/home/ranjith/Documents/bot-transfer/data/';
+const zippath = '/home/ranjith/Documents/bot-transfer/'
+var zipFolder = require('zip-folder');
+ 
+
+// var watcher = chokidar.watch('/home/ranjith/Documents/socket.io-file-example/data', {ignored: /^\./, persistent: true});
+//
+// watcher
+// 	.on('add', function(path) {
+// 		console.log("Server watcher called");
+//
+// 		console.log('File', path, 'has been added');})
+// 	.on('change', function(path) {console.log('File', path, 'has been changed');})
+// 	.on('unlink', function(path) {console.log('File', path, 'has been removed');})
+// 	.on('error', function(error) {console.error('Error happened', error);})
+
+
+
+
+
+io.on('connection', (socket) => {
+	console.log('Socket connected.');
+//Zipping for initial download.
+zipFolder(datapath, zippath + 'archive.zip', function(err) {
+    if(err) {
+        console.log('Zipping Error, Master copy not sent');
+    } else {
+        console.log('Zipping done');
+		var stream = ss.createStream();
+console.log("Sending Master folder");
+fs.createReadStream(zippath + 'archive.zip').pipe(stream);
+ss(socket).emit('master-sent',stream);	
+console.log("Sent Master Folder");
+    }
+});
+
+ 
+
+	var count = 0;
+	console.log(socket.id);
+	hmap.set(socket.id, socket);
+	console.log(hmap);
+	ss(socket).on('client-added', function(stream, data) {
+		console.log("client added");
+		// ss(socket).broadcast.emit('profile-image1', stream, {name: path});
+		var filename = path.basename(data.name);
+    stream.pipe(fs.createWriteStream(datapath + filename))
+		stream.on("end", function(){
+			console.log("end called");
+			hmap.forEach(function(value, key) {
+					console.log("checking socket");
+					if(socket.id != value.id) {
+						console.log("asdfasdfasdfasdfasdfasdf")
+						console.log(key + " : " + value);
+						var stream = ss.createStream();
+						fs.createReadStream(datapath + filename).pipe(stream);
+						ss(value).emit('server-added', stream, {name: filename});
+						
+					}
+			});
+		});
+		
+		hmap.forEach(function(value, key) {
+    console.log(key + " : " + value);
+});
+		
+	})
+//modify file event listener
+		ss(socket).on('client-modified', function(stream, data) {
+		console.log("client modified");
+		// ss(socket).broadcast.emit('profile-image1', stream, {name: path});
+		var filename = path.basename(data.name);
+    		stream.pipe(fs.createWriteStream(datapath + filename, {flags: 'w'}));
+		stream.on("end", function(){
+			console.log("end called");
+			hmap.forEach(function(value, key) {
+					console.log("checking socket");
+					if(socket.id != value.id) {
+						console.log("asdfasdfasdfasdfasdfasdf")
+						console.log(key + " : " + value);
+						var stream = ss.createStream();
+						fs.createReadStream(datapath + filename).pipe(stream);
+						ss(value).emit('server-modified', stream, {name: filename});
+						
+					}
+			});
+		});
+hmap.forEach(function(value, key) {
+    console.log(key + " : " + value);
+});
+	})
+//remove file event listener
+		ss(socket).on('client-removed', function(data) {
+		console.log("client removed");
+		// ss(socket).broadcast.emit('profile-image1', stream, {name: path});
+		
+		var filename = path.basename(data.name);
+    		fs.unlink(datapath + filename, function(error) {
+   			 if (error) {
+        			console.log("Error:File not deleted on server");
+    				}else{console.log("File deleted on server");}});
+		
+			hmap.forEach(function(value, key) {
+					console.log("checking socket");
+					if(socket.id != value.id) {
+						console.log("asdfasdfasdfasdfasdfasdf")
+						console.log(key + " : " + value);
+						
+						ss(value).emit('server-removed', {name: filename});
+					}
+			});
+		
+hmap.forEach(function(value, key) {
+    console.log(key + " : " + value);
+});
+	})
+socket.on('disconnect', function () {
+	// socketconn.pop(socket.id);
+	hmap.delete(socket.id);
+	 console.log(hmap);
+ });
+})
+
+
+
+
+httpServer.listen(3000, () => {
+	console.log('Server listening on port 3000');
+});
